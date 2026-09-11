abcb9de20e052354666cfaffa0c04227207544db
nachathiraabe-coder <nachathiraabe@gmail.com>
2026-09-11 20:59:10 +0530

    Update add two numbers.java

--- add two numbers.java
+++ add two numbers.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+ public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+   int a = sc.nextInt();
+   int b = sc.nextInt();
 
-        int sum = a + b;
+   int sum = a + b;
 
-        System.out.println("Sum = " + sum);
+   System.out.println("Sum = " + sum);
     }
 }
