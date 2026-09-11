93986e48dbe6d24b5db0ebc0743959fb63a00c61
nachathiraabe-coder <nachathiraabe@gmail.com>
2026-09-11 21:01:33 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-   Scanner sc = new Scanner(System.in);
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
 
-      int num = sc.nextInt();
-      int sum = 0;
+    int num = sc.nextInt();
+    int sum = 0;
 
       while (num > 0) {
-            int digit = num % 10;
-            sum = sum + digit;
-            num = num / 10;
+         int digit = num % 10;
+          sum = sum + digit;
+          num = num / 10;
         }
 
-    System.out.println("Sum of digits: " + sum);
+   System.out.println("Sum of digits: " + sum);
     }
 }
