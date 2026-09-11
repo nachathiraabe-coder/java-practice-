9c376d0638d84e22e51d323732587a6c2880daf4
nachathiraabe-coder <nachathiraabe@gmail.com>
2026-09-11 20:58:06 +0530

    Update SumOfDigits.java

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+   Scanner sc = new Scanner(System.in);
 
       int num = sc.nextInt();
       int sum = 0;
